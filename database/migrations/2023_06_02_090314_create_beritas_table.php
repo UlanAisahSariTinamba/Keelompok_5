4f6e297a20d73816ded5db8f1fa0bd7e135ea2e6
UlanAisahSariTinamba <ulantinamba2909@gmail.com>
2024-07-26 04:31:00 +0900

    migrtation

---
+++ database/migrations/2023_06_02_090314_create_beritas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kategori_berita_id');
+            $table->foreignId('user_id');
+            $table->string('judul');
+            $table->string('slug')->unique();
+            $table->text('isi_berita');
+            $table->timestamp('published_at')->nullable();
+            $table->string('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('berita');
+    }
+};
